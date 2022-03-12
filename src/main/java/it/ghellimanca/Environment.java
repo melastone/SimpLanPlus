6692a25bfea479d71dddde47c63c76a2ec6ec7b9
melastone <melania.ghelli@studio.unibo.it>
2022-03-12 09:58:48 +0100

    update code before branching

--- src/main/java/it/ghellimanca/Environment.java
+++ src/main/java/it/ghellimanca/Environment.java
@@ -1,4 +1,23 @@
 package it.ghellimanca;
 
+/*
+ * This class represents the Symbol Table.
+ *
+ * It is implemented as a list of hashtables.
+ *
+ * The list is treated as a stack (methods use the pop/push operations).
+ *
+ */
+
 public class Environment {
+
+    /*
+     * Modifiers:
+     * final cause it's not supposed to be overridden
+     * private because outside of this class the code interacts
+     * with the table via Environment class methods.
+     *
+     */
+    final private  symbolTable
+
 }
