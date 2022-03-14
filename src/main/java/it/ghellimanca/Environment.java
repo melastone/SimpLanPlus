4f0f1d70e9039df6bb5735342403c76b60f962a5
melastone <melania.ghelli@studio.unibo.it>
2022-03-14 23:09:10 +0100

    scopeEntry() and scopeExit() defined

--- src/main/java/it/ghellimanca/Environment.java
+++ src/main/java/it/ghellimanca/Environment.java
@@ -1,14 +1,11 @@
 package it.ghellimanca;
 
-/*
+/**
  * This class represents the Symbol Table.
- *
  * It is implemented as a list of hashtables.
- *
- * The list is treated as a stack (methods use the pop/push operations).
- *
  */
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,21 +22,59 @@ public class Environment {
      */
     final private List<Map<String,STEntry>> symbolTable ;
 
+    private int nestingLevel;
 
-    public List<Map<String,STEntry>> newScope()
-    // extends the symbolTable with a new empty scope
 
-    public List<Map<String,STEntry>> newScope(Map<String,STEntry> s)
-    // extends the symbolTable with an initialized scope s
+    /**
+     * Constructor for {@code Environment}
+     *
+     * @param symTable     a list of hashmaps (that can be empty)
+     * @param nestingLevel a positive integer
+     */
+    public Environment(List<Map<String, STEntry>> symTable, int nestingLevel) {
+        this.symbolTable = symTable;
+        this.nestingLevel = nestingLevel;
+    }
 
+
+    /**
+     * Extends the symbolTable with a new empty scope
+     */
+    public List<Map<String,STEntry>> newScope() {
+        symbolTable.add(new HashMap<>());
+        nestingLevel += 1;
+    }
+
+
+    /**
+     * Extends the symbolTable with an initialized scope
+     * @param  s  the scope to add
+     */
+    public List<Map<String,STEntry>> newScope(Map<String,STEntry> s) {
+        symbolTable.add(s);
+        nestingLevel += 1;
+    }
+
+
+    /*
+     * if there is no clash of names, adds id ⟼ t to st
+     * if it find a collision needs to throw an exception
+     */
     public List<Map<String,STEntry>> addDecl(String id, Type t)
-    // if there is no clash of names, adds id ⟼ t to st
-    // if it find a collision needs to throw an exception
 
+
+    /*
+     looks for the type of id in st, if any
+     */
     Type lookup(String id)
-    // looks for the type of id in st, if any
 
-    public List<Map<String,STEntry>> exitScope()
-    // exits the current scope
+
+    /**
+     * Exits the current scope
+     */
+    public List<Map<String,STEntry>> exitScope() {
+        symbolTable.remove(nestingLevel);
+        nestingLevel--;
+    }
 
 }
