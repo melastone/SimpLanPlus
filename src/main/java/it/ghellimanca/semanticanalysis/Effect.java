9694bd404e90bdb69d82fd5ff5c9ed0f15f8a771
melastone <melania.ghelli@studio.unibo.it>
2022-10-07 15:53:21 +0200

    created Effect class and an example for effect analysis

---
+++ src/main/java/it/ghellimanca/semanticanalysis/Effect.java
@@ -0,0 +1,24 @@
+package it.ghellimanca.semanticanalysis;
+
+/**
+ * Represents an effect for a variable.
+ * It is used for Effect Analysis.
+ */
+public class Effect {
+
+    private static final int DECLARED = 0;  //  ⊥
+    private static final int INIT = 1;
+    private static final int USED = 2;
+    private static final int ERROR = 3;     // ⊤
+
+    private final int status;
+
+
+    public Effect(int status) {
+        this.status = status;
+    }
+
+    public Effect() {
+        this(DECLARED);
+    }
+}
